a1fac876b99ffca2fe1909b9365fcc125bc47f8a
George Roberts <george.roberts1@vodafone.com>
2022-03-17 14:25:11 +0000

    Added cli commands

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello from remote Lambda functionssss!"),
+    body: JSON.stringify("Hello from remote Lambda function!"),
   };
   return response;
 };
