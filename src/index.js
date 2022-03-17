2db87eab64e0aacf221dcb222cdd09d0a89954f7
George Roberts <george.roberts1@vodafone.com>
2022-03-17 16:12:24 +0000

    Test github action

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello from remote Lambda FACTIONS!"),
+    body: JSON.stringify("Hello from remote Lambda functioneers!"),
   };
   return response;
 };
